936d8f02b10e8192be91feb73a3f203f6b5019e2
Ashraj Grewal <ashraj.grewal10@gmail.com>
2020-09-15 16:23:24 -0400

    Using input function

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
-import sys
-
+#Custom Calculator
+#Here I define the function 
 def division(x, y):
-    answer = x / y
+    #Perform calculation (division)
+    answer = int(x) / int(y)
+    #Print out the answer for the user
     print(answer)
 
-division(int(sys.argv[1]), int(sys.argv[2]))
+#In lines 8 and 9 I ask for an input from the user
+x = input("Enter the first number:")
+y = input("Enter the second number:")
+#In line 11 I call the function and pass x and y as parameters 
+division(x, y)
