81a403158a6b30b8664dc46ab68c1c574e24a9ea
Ashraj Grewal <ashraj.grewal10@gmail.com>
2020-09-18 00:00:00 -0400

    Update app.py

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ def division(x, y):
     #Print out the answer for the user
     print(answer)
 
-#In lines 8 and 9 I ask for an input from the user
+#In lines 10 and 11 I ask for an input from the user
 x = input("Enter the first number:")
 y = input("Enter the second number:")
-#In line 11 I call the function and pass x and y as parameters 
+#In line 13 I call the function and pass x and y as parameters 
 division(x, y)
